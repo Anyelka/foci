6f970d995c6406c6647f6de31ec49086bbdf94e7
Marton Takacs <marton.takacs@tesco.com>
2024-08-30 16:44:45 +0200

    first 8 formations

--- src/components/players/Player.jsx
+++ src/components/players/Player.jsx
@@ -3,20 +3,22 @@ import Shirt from "./../../assets/shirt-black.svg";
 
 const Player = ({ name, onHover, onClick }) => {
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 50 }}
-        className="player"
-        whileHover={onHover}
-        onClick={onClick}
-        layout
-      >
-        <motion.img className="player-shirt" src={Shirt} alt="" />
-        <div className="player-name">{name}</div>
-      </motion.div>
-    </AnimatePresence>
+    name && (
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 50 }}
+          className="player"
+          whileHover={onHover}
+          onClick={onClick}
+          layout
+        >
+          <motion.img className="player-shirt" src={Shirt} alt="" />
+          <div className="player-name">{name}</div>
+        </motion.div>
+      </AnimatePresence>
+    )
   );
 };
 
