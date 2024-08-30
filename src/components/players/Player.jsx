dcf53c72e8b24b152636a2896209821e969ea504
Marton Takacs <marton.takacs@tesco.com>
2024-08-30 15:17:31 +0200

    layout & presence animation for players

--- src/components/players/Player.jsx
+++ src/components/players/Player.jsx
@@ -1,12 +1,22 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Shirt from "./../../assets/shirt-black.svg";
 
 const Player = ({ name, onHover, onClick }) => {
   return (
-    <motion.div className="player" whileHover={onHover} onClick={onClick}>
-      <motion.img className="player-shirt" src={Shirt} alt="" />
-      <div className="player-name">{name}</div>
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 50 }}
+        className="player"
+        whileHover={onHover}
+        onClick={onClick}
+        layout
+      >
+        <motion.img className="player-shirt" src={Shirt} alt="" />
+        <div className="player-name">{name}</div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
