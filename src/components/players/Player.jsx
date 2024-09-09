72bdcda7e344e62694018dfd4f660d9930872e91
Marton Takacs <marton.takacs@tesco.com>
2024-09-09 09:28:06 +0200

    center icon inside player shirt

--- src/components/players/Player.jsx
+++ src/components/players/Player.jsx
@@ -1,21 +1,46 @@
 import { motion } from "framer-motion";
+import Add from "./../../assets/add.svg";
 import Shirt from "./../../assets/shirt-black.svg";
 
 const Player = ({ id, name, onHoverFilter, onClick }) => {
+  const playerVariants = {
+    initial: { scale: 1 },
+    animateHover: {
+      scale: 1.1,
+      cursor: "pointer",
+      filter: onHoverFilter,
+    },
+  };
+
+  const actionIconVariants = {
+    initial: { scale: 1, rotateY: 0 },
+    animateHover: { scale: 1, rotateY: 360 },
+  };
+
   return (
     <motion.div
       id={`player-${id}`}
       className="player"
-      whileHover={{
-        scale: 1.1,
-        cursor: "pointer",
-        filter: onHoverFilter,
-      }}
       onClick={onClick}
+      variants={playerVariants}
+      initial="initial"
+      whileHover="animateHover"
       layoutId={`player-${id}`}
       layout
     >
-      <motion.img className="player-shirt" src={Shirt} alt="" />
+      <div className="player-icon-container">
+        <motion.img
+          className="player-shirt-icon"
+          src={Shirt}
+          alt=""
+        ></motion.img>
+        <img
+          className="player-action-icon"
+          /* variants={actionIconVariants} */ src={Add}
+          alt=""
+        ></img>
+      </div>
+
       <div className="player-name">{name}</div>
     </motion.div>
   );
